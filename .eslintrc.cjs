072460e0d8a10586a22b81a8c04c5bc9bb485b20
Umair Umair <umair@deinarzt.digital>
2025-08-24 17:31:23 +0200

    Initi Vite and React

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+    root: true, 
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+        'react',
+        'react-hooks'
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended'
+    ],
+    env:{
+        browser:true,
+        es2021:true,
+    },
+    settings:{
+        react: {
+            version: 'detect',
+        },
+    },
+    rules:{
+        'react/react-in-jsx-scope': 'off', 
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended'
+      ],
+      
+}
